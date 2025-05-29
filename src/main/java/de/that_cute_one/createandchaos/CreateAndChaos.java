bfc0fe852efb9897ba2bb6bffb81bf820e2efaa2
ThatCuteOne <105042017+ThatCuteOne@users.noreply.github.com>
2025-05-29 04:02:37 +0200

    - move item settings into seperate class - added intermidiets for all weapons except the new ones - tweaked gradle

--- src/main/java/de/that_cute_one/createandchaos/CreateAndChaos.java
+++ src/main/java/de/that_cute_one/createandchaos/CreateAndChaos.java
@@ -2,6 +2,7 @@ package de.that_cute_one.createandchaos;
 
 import net.fabricmc.api.ModInitializer;
 
+import de.that_cute_one.createandchaos.ModItems;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -24,9 +25,6 @@ public class CreateAndChaos implements ModInitializer {
 	public static FlowableFluid FLOWING_NAPHTHA;
 	public static Item NAPHTHA_BUCKET;
 	public static Block NAPHTHA;
-	public static final Item GUN_MECHANISM = new Item(new FabricItemSettings().maxCount(64));
-	public static final Item INCOMPLETE_GUN_MECHANISM = new Item(new FabricItemSettings().maxCount(1));
-
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -37,8 +35,28 @@ public class CreateAndChaos implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_gun_mechanism"), INCOMPLETE_GUN_MECHANISM);
-		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "gun_mechanism"), GUN_MECHANISM);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_pistol_light"), ModItems.INCOMPLETE_PISTOL_LIGHT);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_pistol_heavy"), ModItems.INCOMPLETE_PISTOL_HEAVY);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_revolver_magnum"), ModItems.INCOMPLETE_REVOLVER_MAGNUM);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_revolver_coltarmy"), ModItems.INCOMPLETE_REVOLVER_COLTARMY);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_smg_machine_pistol"), ModItems.INCOMPLETE_SMG_MACHINE_PISTOL);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_smg_heavy"), ModItems.INCOMPLETE_SMG_HEAVY);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_smg_rapid"), ModItems.INCOMPLETE_SMG_RAPID);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_assaultrifle_light"), ModItems.INCOMPLETE_ASSAULTRIFLE_LIGHT);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_assaultrifle_heavy"), ModItems.INCOMPLETE_ASSAULTRIFLE_HEAVY);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_assaultrifle_rus"), ModItems.INCOMPLETE_ASSAULTRIFLE_RUS);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_shotgun_doublebarrel"), ModItems.INCOMPLETE_SHOTGUN_DOUBLEBARREL);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_shotgun_combat"), ModItems.INCOMPLETE_SHOTGUN_COMBAT);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_shotgun_riot"), ModItems.INCOMPLETE_SHOTGUN_RIOT);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_sniper_classic"), ModItems.INCOMPLETE_SNIPER_CLASSIC);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_sniper_cowboy"), ModItems.INCOMPLETE_SNIPER_COWBOY);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_sniper_marksman"), ModItems.INCOMPLETE_SNIPER_MARKSMAN);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_lmg_m60"), ModItems.INCOMPLETE_LMG_M60);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_amr_classic"), ModItems.INCOMPLETE_AMR_CLASSIC);
+
+
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "incomplete_gun_mechanism"), ModItems.INCOMPLETE_GUN_MECHANISM);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "gun_mechanism"), ModItems.GUN_MECHANISM);
 		STILL_NAPHTHA = Registry.register(Registries.FLUID, new Identifier(MOD_ID, "naphtha"), new NaphthaFluid.Still());
 		FLOWING_NAPHTHA = Registry.register(Registries.FLUID, new Identifier(MOD_ID, "flowing_naphtha"), new NaphthaFluid.Flowing());
 		NAPHTHA_BUCKET = Registry.register(Registries.ITEM, new Identifier(MOD_ID, "naphtha_bucket"),
---
+++ src/main/java/de/that_cute_one/createandchaos/ModItems.java
@@ -0,0 +1,27 @@
+package de.that_cute_one.createandchaos;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+
+public class ModItems {
+	public static final Item GUN_MECHANISM = new Item(new FabricItemSettings().maxCount(64));
+	public static final Item INCOMPLETE_GUN_MECHANISM = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_PISTOL_LIGHT = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_PISTOL_HEAVY = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_REVOLVER_MAGNUM = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_REVOLVER_COLTARMY = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SMG_MACHINE_PISTOL = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SMG_HEAVY = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SMG_RAPID = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_ASSAULTRIFLE_LIGHT = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_ASSAULTRIFLE_HEAVY = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_ASSAULTRIFLE_RUS = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SHOTGUN_DOUBLEBARREL = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SHOTGUN_COMBAT = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SHOTGUN_RIOT = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SNIPER_CLASSIC = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SNIPER_COWBOY = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_SNIPER_MARKSMAN = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_LMG_M60 = new Item(new FabricItemSettings().maxCount(1));
+	public static final Item INCOMPLETE_AMR_CLASSIC= new Item(new FabricItemSettings().maxCount(1));
+}
